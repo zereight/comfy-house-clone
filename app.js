017ed1b984bd9735414d4a1bac9c66e015a776cc
zereight <zereight@zereightui-MacBookPro.local>
2020-08-07 21:18:47 +0900

    final

--- app.js
+++ app.js
@@ -5,4 +5,33 @@ const closeCartBtn = document.querySelector(".close-cart");
 const clearCartBtn = document.querySelector(".clear-cart");
 const cartDOM = document.querySelector(".cart");
 const cartOverlay = document.querySelector(".cart-overlay");
-const cartItems = document.querySelector(".cart-content");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// cart
+let cart = [];
+
+// getting the products
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+// display products
+class UI {}
+
+// local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+});
