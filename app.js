a43f9ef09a4a1ec414f3f5cdd7e91d620a6a2722
zereight <zereight@zereightui-MacBookPro.local>
2020-08-07 15:34:53 +0900

    html,css

---
+++ app.js
@@ -0,0 +1,8 @@
+// variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-content");
